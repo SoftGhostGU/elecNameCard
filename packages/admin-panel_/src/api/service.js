b1ab2b8c45a6adf6d7af935346bb3e566b116b15
SoftGhostGU <163658635+SoftGhostGU@users.noreply.github.com>
2025-08-19 23:55:18 +0800

    feat: 几个工具类的编写

---
+++ packages/admin-panel_/src/api/service.js
@@ -0,0 +1,6 @@
+export const service = {
+	login: 'api/admin/login', //登录
+	allUsers: 'api/user/queryAllUserInfo', //获取所有用户
+	users: 'api/user/user', //增删改查用户
+	allLogs: 'api/admin/queryAllLogs', //获取所有日志
+}
